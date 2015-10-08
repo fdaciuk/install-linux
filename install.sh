7b45703f576e6f9c7de18158f688b100a211deff
Fernando Daciuk <fdaciuk@users.noreply.github.com>
2015-10-08 06:55:01 -0300

    Remove comments

--- install.sh
+++ install.sh
@@ -9,25 +9,21 @@ ok() {
   echo "→ "$1" OK"
 }
 
-# Update system
 sudo apt-get update 
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 ok "System updated!"
 
-# Curl
 if not curl; then
   sudo apt-get install curl
 fi
 ok "Curl"
 
-# Vim
 if not vim; then
   sudo apt-get install vim -y
 fi
 ok "Vim"
 
-# Git
 if not git; then
   sudo add-apt-repository -y ppa:git-core/ppa
   sudo apt-get update
@@ -35,7 +31,6 @@ if not git; then
 fi
 ok "Git"
 
-# Atom
 if not atom; then
   sudo add-apt-repository -y ppa:webupd8team/atom
   sudo apt-get update
@@ -43,7 +38,6 @@ if not atom; then
 fi
 ok "Atom"
 
-# Chrome
 if not google-chrome-stable; then
   wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg --force-depends -i chrome.deb
@@ -52,14 +46,12 @@ if not google-chrome-stable; then
 fi
 ok "Chrome"
 
-# Docker
 if not docker; then
   wget -qO- https://get.docker.com/ | sh
   sudo usermod -aG docker $(whoami)
 fi
 ok "Docker"
 
-# KeePassX
 if not keepassx; then
   sudo add-apt-repository -y ppa:keepassx/ppa
   sudo apt-get update
@@ -67,7 +59,6 @@ if not keepassx; then
 fi
 ok "KeePassX"
 
-# Dropbox
 if not dropbox; then
   bash -c "$(https://raw.githubusercontent.com/fdaciuk/elementary-dropbox/master/install.sh)"
 fi
