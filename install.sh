80a08289aea012448529b2ce87e8116d7ac2d4e0
Fernando Daciuk <fdaciuk@users.noreply.github.com>
2015-10-26 21:42:48 -0200

    Add correct keepassx path and dropbox

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
-not() {
-  ! [[ -x "$(command -v "$1")" ]] ;
+has() {
+  [[ -x "$(command -v "$1")" ]] ;
+}
+
+has_not() {
+  ! has "$1" ;
 }
 
 ok() {
@@ -14,31 +18,31 @@ sudo apt-get autoclean -y
 sudo apt-get update
 ok "System updated!"
 
-if not curl; then
+if has_not curl; then
   sudo apt-get install curl
 fi
 ok "Curl"
 
-if not vim; then
+if has_not vim; then
   sudo apt-get install vim -y
 fi
 ok "Vim"
 
-if not git; then
+if has_not git; then
   sudo add-apt-repository -y ppa:git-core/ppa
   sudo apt-get update
   sudo apt-get install git -y
 fi
 ok "Git"
 
-if not atom; then
+if has_not atom; then
   sudo add-apt-repository -y ppa:webupd8team/atom
   sudo apt-get update
   sudo apt-get install atom -y
 fi
 ok "Atom"
 
-if not google-chrome-stable; then
+if has_not google-chrome-stable; then
   wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg --force-depends -i chrome.deb
   rm chrome.deb
@@ -46,20 +50,18 @@ if not google-chrome-stable; then
 fi
 ok "Chrome"
 
-if not docker; then
+if has_not docker; then
   wget -qO- https://get.docker.com/ | sh
   sudo usermod -aG docker $(whoami)
 fi
 ok "Docker"
 
-if not keepassx; then
-  sudo add-apt-repository -y ppa:keepassx/ppa
-  sudo apt-get update
+if has_not keepassx; then
   sudo apt-get install keepassx -y
 fi
 ok "KeePassX"
 
-if not dropbox; then
-  bash -c "$(https://raw.githubusercontent.com/fdaciuk/elementary-dropbox/master/install.sh)"
+if has nautilus && has_not dropbox; then
+  sudo apt-get install nautilus-dropbox -y
 fi
 ok "Dropbox"
