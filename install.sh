d1cff90313bf2fae197311bec1f48478bf1fd580
Fernando Daciuk <fdaciuk@users.noreply.github.com>
2015-11-06 10:03:30 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 has() {
-  [[ -x "$(command -v "$1")" ]] ;
+  [[ -x "$(command -v "$1")" ]];
 }
 
 has_not() {
@@ -97,6 +97,18 @@ if has_not skype; then
   rm -rf skype-extension*
 fi
 ok "Skype"
+
+if ! [[ -d "/opt/stremio" ]]; then
+  curl -#So http://178.62.254.47/Stremio3.3.1.linux.tar.gz
+  sudo mkdir -p /opt/stremio
+  sudo tar -xvzf Stremio3.3.1.linux.tar.gz -C /opt/stremio
+  curl -SO# http://www.strem.io/3.0/stremio-white-small.png
+  sudo mv stremio-white-small.png /opt/stremio/
+  curl -SO# https://gist.githubusercontent.com/claudiosmweb/797b502bc095dabee606/raw/52ad06b73d90a4ef389a384fbc815066c89798eb/stremio.desktop
+  sudo mv stremio.desktop /usr/share/applications/
+  rm Stremio3.3.1.linux.tar.gz
+fi
+ok "Stremio"
 
 # Clean up
 sudo apt-get autoclean -y
