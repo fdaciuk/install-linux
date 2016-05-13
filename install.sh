cbe6845c7b8f49edf7701eb005cf5d9a4e3ef1d0
Fernando Daciuk <fdaciuk@users.noreply.github.com>
2016-05-13 00:25:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,15 +58,11 @@ fi
 ok "Chrome"
 
 if has_not docker; then
-  sudo apt-get install apt-transport-https ca-certificates
+  sudo apt-get install apt-transport-https ca-certificates linux-image-extra-$(uname -r) -y
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -sc) main' | cat > /etc/apt/sources.list.d/docker.list"
   sudo apt-get update
   sudo apt-get purge lxc-docker
-  apt-cache policy docker-engine
-  sudo apt-get update
-  sudo apt-get install linux-image-extra-$(uname -r) -y
-  sudo apt-get update
   sudo apt-get install docker-engine -y
   sudo service docker start
 fi
