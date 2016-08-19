47053f4dfd1bb8b29c90d5c2c67f9cc511bed014
Fernando Daciuk <fdaciuk@users.noreply.github.com>
2016-08-19 13:57:24 -0300

    Update qualcomm-atheros-wireless.sh

--- qualcomm-atheros-wireless.sh
+++ qualcomm-atheros-wireless.sh
@@ -5,5 +5,5 @@ sudo mkdir -p /lib/firmware/ath10k/QCA6174/hw3.0/
 sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/* 2> /dev/null
 sudo wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board.bin https://github.com/FireWalkerX/ath10k-firmware/blob/7e56cbb94182a2fdab110cf5bfeded8fd1d44d30/QCA6174/hw3.0/board-2.bin?raw=true
 sudo wget -O /lib/firmware/ath10k/QCA6174/hw3.0/firmware-4.bin https://github.com/FireWalkerX/ath10k-firmware/blob/7e56cbb94182a2fdab110cf5bfeded8fd1d44d30/QCA6174/hw3.0/firmware-4.bin_WLAN.RM.2.0-00180-QCARMSWPZ-1?raw=true
-sudo chmod +x /lib/firmware/ath10k/QCA6174/hw3.0/*
+sudo chmod +x-w /lib/firmware/ath10k/QCA6174/hw3.0/*
 echo "Restart your computer!"
